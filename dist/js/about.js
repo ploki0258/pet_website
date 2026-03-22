11b097fa76ca699aad110d7d3962b8b47708d856
Jack_Chiu <qqwer12313@gmail.com>
2026-03-22 21:30:50 +0800

    Update_20260322

--- dist/js/about.js
+++ dist/js/about.js
@@ -2,4 +2,4 @@ import { Page } from "./components/Page.js";
 import { PageBanner } from "./components/PageBanner.js";
 
 Page.loader("about");
-PageBanner.loader("#page-banner-endpoint", "關於我們", "「因為愛，我們相遇；讓毛孩不再孤單。」");
+PageBanner.loader("#page-banner-endpoint", "關於我們", "「因為愛，我們相遇，讓毛孩不再孤單。」");
